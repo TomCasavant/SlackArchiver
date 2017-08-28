160764027ff0b22f307c6f18a815db4cd590dd00
Tom <tominthehat@gmail.com>
2017-08-28 14:28:13 -0400

    Cleaned up code

--- main.py
+++ main.py
@@ -1,51 +1,58 @@
-from pymongo import MongoClient
-from slackclient import SlackClient
 from ConfigParser import SafeConfigParser
 import time
+from pymongo import MongoClient
+from slackclient import SlackClient
 
-def logPreviousSlackData(API_Key):
-	"""Gets all previous messages and puts them into a mongo database"""
-	sc = SlackClient(API_Key)
-	list = sc.api_call("channels.list")
-	history = sc.api_call(
-		"channels.history",
-		channel ="C6UD3MMNW")
-	insertDatabase(history['messages'])
 
-def getCurrentMessages(API_Key):
-	"""Actively logs messages into a mongo database"""
-	sc = SlackClient(API_Key)
-	if sc.rtm_connect():
-		while True:
-			messages= sc.rtm_read()
-			time.sleep(1)
-			if messages:
-				insertDatabase(messages)
+def log_previous_slack_data(slackclient, mongoclient):
+    """Gets all previous messages and puts them into a mongo database"""
+    channels = slackclient.api_call("channels.list")
+    print channels
+    history = slackclient.api_call(
+        "channels.history",
+        channel="C6UD3MMNW")
+    insert_database(history['messages'], mongoclient)
 
-def printDatabase():
-	"""Prints contents of database"""
-	client = MongoClient()
-	db = client['Slack-Database']
-	collection = db['posts']
-	a = collection.find({})
-	for object in a:
-		print object['text'], object['user']
 
-def insertDatabase(messages):
-	"""Takes messages and inserts them into a database"""
-	client = MongoClient()
-	db = client['Slack-Database']
-	posts = db.posts
-	for message in messages:
-		if message['type'] == "message":
-			posts.replace_one(message, message, upsert=True)
-			print "Message Logged" 
+def start_listening(slackclient, mongoclient):
+    """Actively logs messages into a mongo database"""
+    if slackclient.rtm_connect():
+        while True:
+            messages = slackclient.rtm_read()
+            if messages:
+                insert_database(messages, mongoclient)
+            time.sleep(1)
+    else:
+        print "Unable to connect"
+
+
+def print_database(client):
+    """Prints contents of database"""
+    database = client['Slack-Database']
+    collection = database['posts']
+    find_all = collection.find({})
+    for document in find_all:
+        print document['text'], document['user']
+
+
+def insert_database(messages, client):
+    """Takes messages and inserts them into a database"""
+    database = client['Slack-Database']
+    posts = database.posts
+    for message in messages:
+        if message['type'] == "message":
+            posts.replace_one(message, message, upsert=True)
+            print "Message Logged"
+
 
 if __name__ == "__main__":
-	parser = SafeConfigParser()
-	parser.read("config.ini") #Loads Config File to retrieve Slack API Token
-	SLACK_API_TOKEN = parser.get("slack", "API_TOKEN")
-#	logPreviousSlackData(SLACK_API_TOKEN)
-#	printDatabase()
-	getCurrentMessages(SLACK_API_TOKEN)
+    parser = SafeConfigParser()
+    parser.read("config.ini")  # Loads Config File to retrieve Slack API Token
+    SLACK_API_TOKEN = parser.get("slack", "API_TOKEN")
 
+    mongo = MongoClient()
+    slack = SlackClient(SLACK_API_TOKEN)
+
+    log_previous_slack_data(slack, mongo)
+    print_database(mongo)
+    start_listening(slack, mongo)
